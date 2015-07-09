548e294f1540417e782ef060e0d69d4022e58511
Maxim Kirdeev <mkirdeev@roox.ru>
2015-07-09 14:31:52 +0300

    run bower_install ad spawn, update dependency

--- tasks/bower_install.js
+++ tasks/bower_install.js
@@ -1,6 +1,14 @@
+
 module.exports = function (grunt) {
 	grunt.registerTask('bower_install', function () {
-		var bw = require('rx-bower-upload');
-		bw();
+		var done = this.async();
+        grunt.util.spawn({
+            cmd: 'node',
+            args: ['node_modules/rx-bower-upload/execute.js'],
+        }, function (error, result, code) {
+            error && grunt.log.error(error);
+            grunt.log.debug(result.stdout);
+            done();
+        });
 	});
 };
