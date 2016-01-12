3c75cb3d5da147595e8537da13e14af404201c05
Maxim Kirdeev <mkirdeev@roox.ru>
2016-01-12 19:30:45 +0300

    support es2015 in feature bundle

--- tasks/helpers/buildFeature.js
+++ tasks/helpers/buildFeature.js
@@ -5,6 +5,8 @@ module.exports = function (grunt, path, featureJson) {
     var aux = path.split('/'),
         concat = grunt.config('concat'),
         features = grunt.config('features'),
+        isES2015 = false,
+        needRunTasks = [],
         featurePath,
         dist;
     aux.pop();
@@ -16,18 +18,34 @@ module.exports = function (grunt, path, featureJson) {
     } catch(e) {
         if ( e.code != 'EEXIST' ) throw e;
     }
+
     (['gadget', 'container']).forEach(function (item) {
         if (featureJson['name'] && featureJson['name'].length && featureJson[item] && featureJson[item].length) {
             if (!features[ featureJson['name'] ]) {
                 features[ featureJson['name'] ] = [];
             }
             features[ featureJson['name'] ].push( dist + '/' + item + '.js' );
-            concat[ dist + '/' +item ] = {
-                src: parseScriptItem(grunt, featurePath, featureJson[item][0]),
-                dest: dist + '/' + item + '.js'
-            };
+            if (featureJson[item][0] &&
+                featureJson[item][0]['$'] &&
+                featureJson[item][0]['$']['syntax'] &&
+                featureJson[item][0]['$']['syntax'] === 'es2015') {
+                var files = {};
+                files[ dist + '/' + item + '.js' ] = parseScriptItem(grunt, featurePath, featureJson[item][0]);
+                grunt.config.set('browserify.feature_' + item + '.files', files);
+                isES2015 = true;
+                needRunTasks.push('browserify:feature_' + item);
+            } else {
+                concat[ dist + '/' +item ] = {
+                    src: parseScriptItem(grunt, featurePath, featureJson[item][0]),
+                    dest: dist + '/' + item + '.js'
+                };
+            }
         }
     });
+
+    if (isES2015) {
+        grunt.task.run(needRunTasks);
+    }
     grunt.config.set('features', features);
     grunt.config.set('concat', concat);
 };
