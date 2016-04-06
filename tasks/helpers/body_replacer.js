46bdee649503546c95c56c564ce937d4a5b0f3f4
Nagaev Maksim <mnagaev@roox.ru>
2016-04-06 20:24:55 +0300

    Fixed problem in configuration substitution Bump version

--- tasks/helpers/body_replacer.js
+++ tasks/helpers/body_replacer.js
@@ -1,9 +1,9 @@
-module.exports = function (body, conf, options, widgetConfig) {
+module.exports = function (body, conf, widgetConfig) {
 	//@todo improve extensibility, finish generic config substution
 	var replaced = body.replace(/__MODULE_ID__/g,conf.mid);
-	var wrsConfig = widgetConfig["com.rooxteam.config"]);
-	if (wrsConfig.CDN_URL) {
-		replaced = replaced.replace(/__CONFIG_CDN_URL__/g, wrsConfig.CDN_URL);
+	var wrsConfig = JSON.parse(widgetConfig)["com.rooxteam.config"];
+	if (wrsConfig['CDN_URL']) {
+		replaced = replaced.replace(/__CONFIG_CDN_URL__/g, wrsConfig['CDN_URL']);
 	}
 	return replaced;
 };
